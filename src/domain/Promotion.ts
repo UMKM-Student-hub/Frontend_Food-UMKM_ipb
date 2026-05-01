76b3ecca840df598e948b62d962db808a760408c
raditbeep <rradit885@gmail.com>
2026-05-01 17:50:38 +0700

    refactor: promo domain

--- src/domain/Promotion.ts
+++ src/domain/Promotion.ts
@@ -5,6 +5,7 @@ export interface Promotion {
   umkm_id: number;
   menu_item_id: number;
   name: string;
+  photo_url: string | null;
   discount_type: DiscountType;
   discount_value: number;
   start_date: string;
@@ -15,6 +16,7 @@ export interface Promotion {
 export interface CreatePromoRequest {
   menu_item_id: number;
   name: string;
+  photo_url?: string;
   discount_type: DiscountType;
   discount_value: number;
   start_date: string;
