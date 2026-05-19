b6d14913fb6d507c54f999653d84d15a7288a964
raditbeep <rradit885@gmail.com>
2026-05-19 23:37:26 +0700

    refactor: remove deprecated pages and components

--- src/pages/auth/LoginPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default class LoginPage extends React.Component {
-  render() {
-    return <div>Halaman Login (Test)</div>;
-  }
-}
--- src/pages/buyer/DealsPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default class DealsPage extends React.Component {
-  render() {
-    return <div>Halaman Promo (Test)</div>;
-  }
-}
